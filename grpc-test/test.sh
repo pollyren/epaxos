1d7903bf9592f626a2daca3e23353db1ee1d5707
Polly Ren <pollyren@uchicago.edu>
2025-11-02 19:55:04 -0500

    change port for server1 to prevent segfault; kill server at the end of the simulation

--- grpc-test/test.sh
+++ grpc-test/test.sh
@@ -1,19 +1,23 @@
-rm-rf build
+rm -rf build
 rm nohup.out
 cmake -S . -B build -DCMAKE_PREFIX_PATH=/opt/homebrew
 cmake --build build -j
 
 
 # three servers (each knows the other two)
-nohup ./build/server e --name=S1 --port=50051 --peers=localhost:50052,localhost:50053 &
-nohup ./build/server e --name=S2 --port=50052 --peers=localhost:50051,localhost:50053 &
-nohup ./build/server e --name=S3 --port=50053 --peers=localhost:50051,localhost:50052 &
+nohup ./build/server e --name=S1 --port=50054 --peers=localhost:50052,localhost:50053 &
+nohup ./build/server e --name=S2 --port=50052 --peers=localhost:50054,localhost:50053 &
+nohup ./build/server e --name=S3 --port=50053 --peers=localhost:50054,localhost:50052 &
 
 # client (with broadcast)
 #nohup ./build/client b --fanout localhost:50051 "hello with fanout"
-sleep 1
-nohup ./build/client w "k121" "apple" localhost:50051 
-#nohup ./build/client g localhost:50051  
+sleep 3
+nohup ./build/client w "k121" "apple" localhost:50054
+nohup ./build/client w "k121" "apple" localhost:50054
+nohup ./build/client w "k121" "apple" localhost:50054
+nohup ./build/client g localhost:50054
 
+# close server
+pkill -f ./build/server
 
 # sample output:
